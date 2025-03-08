e0d2a59d196dd45e7ef98e9a30acf55148ea6cec
Mazingerz969 <billykantabro@gmail.com>
2025-03-08 15:41:16 +0100

    Corregir configuración para despliegue

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,69 +1,20 @@
 /* eslint-disable no-restricted-globals */
-const CACHE_NAME = 'agenda-semanal-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/js/main.chunk.js',
-  '/static/js/0.chunk.js',
-  '/static/js/bundle.js',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
+// Este archivo está intencionalmente vacío para evitar errores durante el despliegue
+// El Service Worker real se implementará en una versión futura
 
-// Instalación del Service Worker
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.__WB_MANIFEST = self.__WB_MANIFEST || [];
+
+// Evento de instalación vacío
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
-// Estrategia de caché: Cache first, then network
+// Evento de activación vacío
+self.addEventListener('activate', () => {
+  self.clients.claim();
+});
+
+// Evento de fetch que simplemente pasa las solicitudes a la red
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-          .then((response) => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone the response
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          });
-      })
-  );
-});
-
-// Actualización del Service Worker
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
